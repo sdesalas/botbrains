c0b656091a6f9c6ec2b3555e283515fb87b8c6f2
Steven de Salas <steven@desalasworks.com>
2017-09-12 19:54:33 +1000

    Mobile improvements

--- quickstart.js
+++ quickstart.js
@@ -1,8 +1,8 @@
 const botbrain = require('./');
 
-const network = new botbrain.NeuralNetwork(160, 'ring');
+const network = new botbrain.NeuralNetwork(60, 'tube');
 
-const output = network.output('OUTPUT', [155,156]);
+const output = network.output('OUTPUT', [55,56, 57]);
 const input = network.input('INPUT', [3,5,7,9]);
 
 //network.join(network, 0.5, 0.5);
